27cb310973bde44a3fccbf6e91381a8354833b2a
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-09-07 14:04:11 -0400

    process env

--- database.js
+++ database.js
@@ -1,22 +1,27 @@
-const { Pool } = require('pg');
+import dotenv from 'dotenv';
+import pg from "pg";
+const { Pool } = pg;
+
+dotenv.config();
 
 // Create a PostgreSQL connection pool
 const pool = new Pool({
-  host: '127.0.0.1',
-  user: 'demo',
-  password: 'demo',
-  database: 'basicapi_users',
-  port: 5432,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
 });
 
+// immediately invoked function expression (IIFE) 
 (async () => {
-  // Connect to the PostgreSQL database
   const client = await pool.connect();
 
   try {
-    // Execute your SQL query here
     const result = await client.query('SELECT * FROM users');
+
     console.log(result.rows);
+
   } catch (error) {
     console.error('Error executing SQL query:', error);
   } finally {
